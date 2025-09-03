ce89b6b2be37810b098da252fbe3f149487c3085
hippie68 <65259318+hippie68@users.noreply.github.com>
2025-09-03 20:25:04 +0200

    GoldHEN v2.4b18.6

---
+++ experimental/75x/changelog.js
@@ -0,0 +1,103 @@
+let changes=`2025-09-03: Added GoldHEN 2.4b18.6.
+2025-08-04:
+  - Added GoldHEN 2.4b18.5.
+  - Updated PS4Debug (v1.1.19).
+2024-05-27:
+  - Updated FTP payload (v1.08b).
+  - Updated PS4Debug (v1.1.16).
+2023-06-13: Updated FTP payload (v1.08a - bug fix for WinSCP).
+2023-03-06: Updated FTP payload (v1.08).
+2023-01-24: Added GoldHEN 2.3 (Debug/Cheat/KLog Settings, GoldHEN shortcut, PS2
+  cheat support, southbridge info, improved app version detection, plugins support, FPS
+  counter, Title ID label feature, scanlines overlay, internal PKG installation support).
+2022-07-31: Updated FTP payload to v1.07 (many bug fixes and improvements).
+2022-03-24: Added GoldHEN 2.2.2 (absolute cheat offsets & bug fixes).
+2022-03-22: Added GoldHEN 2.2.1 (improved cheat menu stability).
+2022-03-10: Added GoldHEN 2.2 (experimental cheat menu).
+2022-02-18: Added GoldHEN 2.1.2 (fixes config parser).
+2022-02-10: Added GoldHEN 2.1.1 (fixes multiple injections).
+2022-02-08: Updated FTP payload (fixes resuming large files).
+2022-02-02:
+  - Added GoldHEN 2.1; removed GoldHEN 2.0b.
+  - Updated Orbis Toolbox.
+2021-12-24: Added GoldHEN 2.0b2.
+2021-12-07: Updated FTP payload (fixes resuming decrypted files).
+2021-12-05:
+  - Self-hosters can now easily add new payloads and wallpapers by creating the file
+    "custom.js". For detailed instructions please consult the README.md on GitHub.
+  - Implemented file compression to reduce the website's size by 92% - please reassign
+    existing custom payload chains.
+2021-11-24:
+  - Added GoldHEN 2.0.
+  - Implemented support for GoldHEN 2.0's BinLoader server: instant payload loading,
+    an optional feature that requires the website to be self-hosted via HTTP (not HTTPS).
+  - Added WebKit exploit addresses that may be worth testing.
+  - Added FakeUSB payload.
+  - Recompiled basic payloads with latest public source code.
+2021-11-11: Updated FTP payload to support simultaneous decryptions and "KILL" command.
+2021-11-01: Implemented editable WebKit exploit address.
+2021-10-26: Fixed wallpaper caching not working anymore.
+2021-08-06: Updated Web Activator.
+2021-07-21: Added latest kernel exploit (2021-07-21).
+2021-06-09: Added Orbis Toolbox payload (Alpha Build 1167).
+2021-06-03: Added latest kernel exploit (2021-06-03).
+2021-05-31: Fixed Web Activator not loading.
+2021-05-28: Added latest kernel exploit (2021-05-27).
+2021-05-26: Replaced FTP payload.
+2021-05-25: Fixed several jailbreak history bugs.
+2021-05-21:
+  - Added latest kernel exploit (2021-05-21).
+  - Significantly increased WebKit exploit execution speed between attempts.
+2021-05-18:
+  - Implemented .bin payload launching.
+  - Converted all .js payloads to .bin - please reassign existing custom payload chains.
+  - Fixed Disable Updates payload.
+2021-05-17:
+  - Added GTA 5 mod menus.
+  - Fixed kernel exploit logging.
+  - Fixed Netcat bug.
+2021-05-05:
+  - Added WebRTE payload.
+  - Added GoldHEN version option to reduce clutter.
+2021-05-04:
+  - Implemented multi-payload launching.
+  - Implemented custom payload chains.
+  - Added latest kernel exploit (2021-05-03).
+2021-04-30:
+  - Merged 7.02 and 7.5x HTML - please report any bugs on GitHub.
+  - Added option to switch between Kernel exploits.
+  - Updated Linux payloads.
+2021-04-29:
+  - Fixed log level option.
+  - Fixed 7.5x-specific layout bug.
+  - Implemented payload styles.
+  - Implemented color schemes.
+  - Added option to switch between WebKit exploits.
+  - Wallpaper caching now displays progress.
+2021-04-22: Updated HEN 2.1.3b (fixes freezing upon closing games).
+2021-04-21:
+  - Added HEN 2.1.3b.
+  - Reworked jailbreak history logging.
+2021-04-14: Implemented wallpapers and wallpaper cache.
+2021-04-13: Added Web Activator.
+2021-04-09:
+  - Added GoldHEN 1.1.
+  - Fixed not loading jailbreak on first try.
+2021-04-07:
+  - Improved logging.
+  - Added average attempts and time calculation.
+2021-04-04:
+  - Implemented Jailbreak history and Settings.
+  - Firmware check now ignores spoof.
+  - Added sleirsgoevy's Linux payloads.
+  - Updated Nazky's Linux payloads.
+  - Updated Mira and jb.js with sleirsgoevy's latest changes.
+2021-03-29:
+  - Fixed not going back to main page after successful jailbreak.
+  - The WebKit exploit now resumes after error "Failed to setup a relative read primitive".
+2021-03-28: Added Linux payloads.
+2021-03-27: Added firmware safety checks.`;
+let data=["aHR0cHM6Ly9jb3JzLmJyaWRnZWQuY2Mv","eC1jb3JzLWdyaWRhLWFwaS1rZXk=","NWJlZGE3MGUtMzlhYi00M2Q0LTkwOTAtMjIwZmM1MzNjNzUy"];
+let date="2025-09-03";
+let build="1";
+let targetFirmware="7.5x";
